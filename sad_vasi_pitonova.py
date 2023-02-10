4699c90e852ae59e1c1a2041fa459534674015bd
fancop <113712008+fancop@users.noreply.github.com>
2023-02-10 19:44:11 +0300

    Add files via upload

---
+++ sad_vasi_pitonova.py
@@ -0,0 +1,32 @@
+import random
+
+
+garden = []
+
+order = (
+    ("ирис", "ирис", "ирис", "ирис", "ирис", "ирис", "ирис", "ирис", "ирис", "ирис"),
+    ("роза", "роза", "роза", "роза", "роза", "роза", "роза", "роза", "роза", "роза"),
+    ("пион", "пион", "пион", "пион", "пион", "пион", "пион", "пион", "пион", "пион")
+)
+
+for package in order:
+    for seed in package:
+        garden.append(seed)
+
+weeds = ("борщевик", "крапива", "лопух")
+
+for weed in weeds:
+  for i in range(random.randint(5, 10)):
+    garden.append(weed)
+
+random.shuffle(garden)
+
+print("сад до прополки:", garden)
+print("идентификатор сада до прополки:", id(garden))
+
+for weed in weeds:
+    while weed in garden:
+        garden.remove(weed)
+
+print("---------------------------------------------------------------------------------")
+print("сад после прополки:", garden)
